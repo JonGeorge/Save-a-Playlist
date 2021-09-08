78d40410f6bffdb902e47aff12809bbfed006b1e
Jon George <23463258+JonGeorge@users.noreply.github.com>
2021-09-08 06:48:24 -0400

    Refactor for readability

--- public/login.js
+++ public/login.js
@@ -2,38 +2,35 @@ displayConnectToSpotifyButton();
 
 function displayConnectToSpotifyButton() {
     if(window.config) {
-        const loggedIn = window.config.isLoggedIn;
+        if(window.config.isLoggedIn) {
+            const checkIcon = getCheckIcon(),
+                  span = getSpan("Connected to Spotify"),
+                  connectedToSpotifyDiv = getConnectedStatusElement(checkIcon, span);
 
-        if(loggedIn) {
-            const checkIcon = document.createElement("img");
-            checkIcon.setAttribute("src", "/static/resources/check-mark-green.svg");
-            checkIcon.setAttribute("class", "checkmark")
-
-            const span = document.createElement("span")
-            span.textContent = "Connected to Spotify";
-
-            const connectedToSpotifyDiv = document.createElement("div");
-            connectedToSpotifyDiv.setAttribute("class", "connected-text");
-            connectedToSpotifyDiv.appendChild(checkIcon);
-            connectedToSpotifyDiv.appendChild(span);
-
+            // If 'Connect to Spotify' button is on the page,
+            // replace Connect button with text that says user
+            // is already connected
             const connectToSpotifyBtn = document.getElementById("login");
-
             if(connectToSpotifyBtn) {
                 const parent = connectToSpotifyBtn.parentNode;
                 parent.replaceChild(connectedToSpotifyDiv, connectToSpotifyBtn);
             }
+
+            // Connect to Spotify button is not on the page.
+            // Append the right side header.
             else {
                 const rightHeader = document.getElementsByClassName("right-header")[0];
                 rightHeader.appendChild(connectedToSpotifyDiv);
             }
         }
+        // Current session is not logged in
         else {
             const strong = document.createElement("strong");
             strong.textContent = "Connect to Spotify";
 
             const a = document.createElement("a");
             a.setAttribute("id", "login");
+            a.setAttribute("href", "javascript:void(0);");
             a.appendChild(strong);
 
             a.addEventListener("click", event => {
@@ -42,12 +39,36 @@ function displayConnectToSpotifyButton() {
 
             const rightHeader = document.getElementsByClassName("right-header")[0];
             rightHeader.appendChild(a);
-
         }
     }
+    // Config hasn't loaded yet
     else {
         setTimeout(displayConnectToSpotifyButton, 50);
     }
+}
+
+function getCheckIcon() {
+    const checkIcon = document.createElement("img");
+    checkIcon.setAttribute("src", "/static/resources/check-mark-green.svg");
+    checkIcon.setAttribute("class", "checkmark");
+
+    return checkIcon;
+}
+
+function getSpan(txt) {
+    const span = document.createElement("span");
+    span.textContent = txt;
+
+    return span;
+}
+
+function getConnectedStatusElement(element1, element2) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "connected-text");
+    div.appendChild(element1);
+    div.appendChild(element2);
+
+    return div;
 }
 
 function openLoginWindow(url) {
