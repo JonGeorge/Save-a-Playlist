17fa4b9934badb5bb01e1e25771048f550973a5c
Jon George <23463258+JonGeorge@users.noreply.github.com>
2021-09-26 09:30:17 -0400

    Add comments with additional info and resources

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,3 +1,18 @@
+/**
+ * More info on Webpack config is here:
+ * https://webpack.js.org/configuration/
+ *
+ * CSS Minimizer minifies CSS files
+ * https://github.com/webpack-contrib/css-minimizer-webpack-plugin
+ *
+ * MiniCssExtract extracts CSS into a new file (which is then loaded by css-loader)
+ * https://webpack.js.org/plugins/mini-css-extract-plugin/
+ *
+ * HtmlWebPackPlugin extract html intot a new file (which is then loaded by html-loader
+ * https://webpack.js.org/plugins/html-webpack-plugin/)
+ *
+ */
+
 const path                 = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin   = require("css-minimizer-webpack-plugin");
@@ -16,13 +31,23 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
 });
 
 module.exports = {
+    // Environment - "production" | "developmentt"
     mode: "production",
+
+    // Entry points for dependencies
     entry: ["./public/appConfig.js",
         "./public/login.js",
         "./public/addEventListeners.js"],
+
     output: {
+        // Path where output files will be placed
         path: path.join(__dirname, "dist"),
+
+        // Name for compiled JS file
         filename: "[name].js",
+
+        // Used by url loader to update paths in html
+        // Our app.js contains a "/public" route for static files
         publicPath: "/public/",
     },
     plugins: [cssExtractPlugin, htmlWebpackPlugin],
@@ -37,6 +62,9 @@ module.exports = {
                 use: ["html-loader"],
             },
             {
+                // URL loader does not include img files injected by our JS.
+                // We have included import statements in our JS files for the artifacts.
+                // File loader will add imported files that match the test regex to our build.
                 test: /(alert-circle-outline-red\.svg$)|(check-mark-green\.svg$)/i,
                 use: [
                     {
