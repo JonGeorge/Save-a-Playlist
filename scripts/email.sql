9a0eee9a6aec289edf1781606e765f798ee80446
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-01-01 11:41:53 -0500

    Clean up unused files

--- scripts/email.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE email (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR NOT NULL,
-    created_on TIMESTAMP NOT NULL DEFAULT current_timestamp
-);
--- scripts/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS client (
-    id SERIAL PRIMARY KEY,
-    client_id uuid NOT NULL,
-    spotify_token VARCHAR NOT NULL,
-    created_on TIMESTAMP NOT NULL DEFAULT current_timestamp
-);
